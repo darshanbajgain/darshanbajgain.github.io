a8d6b402da6ff9f0371e16da1a0105589c3d8ccb
darshanbajgain <70639590+darshanbajgain@users.noreply.github.com>
2020-09-02 07:27:36 -0700

    ALL web files

---
+++ shared.js
@@ -0,0 +1,17 @@
+var backdrop = document.querySelector('.backdrop');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+toggleButton.addEventListener('click', function() {
+    /*mobileNav.style.display = 'block';
+    backdrop.style.display = 'block'; */
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
+
+backdrop.addEventListener('click', function() {
+   /* mobileNav.style.display = 'none';
+    backdrop.style.display = 'none'; */
+    mobileNav.classList.remove('open');
+    backdrop.classList.remove('open');
+});
